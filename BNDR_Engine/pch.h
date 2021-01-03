1108b7af3842cb599095c6d6acadb434d7df9adf
ccb2k18 <ccb7tennis@yahoo.com>
2021-01-02 21:28:28 -0800

    updated copyright notice

--- BNDR_Engine/pch.h
+++ BNDR_Engine/pch.h
@@ -1,6 +1,6 @@
 /*MIT License
 
-Copyright (c) 2020 Caleb Christopher Bender
+Copyright (c) 2021 Caleb Christopher Bender
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
